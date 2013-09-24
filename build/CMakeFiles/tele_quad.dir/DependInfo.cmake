479c8bf3398256b160685bad4086f44c498d24ee
Hongfei <wang.3185@osu.edu>
2013-09-25 05:17:24 +0800

    cut the initial pose to make it more efficient

---
+++ build/CMakeFiles/tele_quad.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maomao/MyRepo/HUBO_work/src/tele_quad.cpp" "/home/maomao/MyRepo/HUBO_work/build/CMakeFiles/tele_quad.dir/src/tele_quad.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/tele_quad_maomao.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tele_quad_maomao.dir/src/tele_quad_maomao.cpp.o"
+  "tele_quad_maomao.pdb"
+  "tele_quad_maomao"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tele_quad_maomao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
